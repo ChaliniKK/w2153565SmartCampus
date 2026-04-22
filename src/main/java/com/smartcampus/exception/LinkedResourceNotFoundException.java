36528ec48dd0088883e1ba9393c8b0fe6b67b2cd
Chalini Kaushalya <chalinikaushalya14@gmail.com>
2026-04-22 18:38:08 +0530

    Add: comments

--- src/main/java/com/smartcampus/exception/LinkedResourceNotFoundException.java
+++ src/main/java/com/smartcampus/exception/LinkedResourceNotFoundException.java
@@ -2,7 +2,6 @@ package com.smartcampus.exception;
 
 /**
  * Custom exception thrown when a dependency resource is missing.
- * e.g., Registering a Sensor to a Room that does not exist.
  * Handled by LinkedResourceNotFoundExceptionMapper to return a 422 Unprocessable Entity.
  */
 public class LinkedResourceNotFoundException extends RuntimeException {
