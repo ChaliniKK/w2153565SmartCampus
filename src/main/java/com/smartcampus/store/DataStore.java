838e391544c116cbcc807234ac2ac882431a96f8
Chalini Kaushalya <chalinikaushalya14@gmail.com>
2026-04-22 18:44:21 +0530

    Update DataStore.java

--- src/main/java/com/smartcampus/store/DataStore.java
+++ src/main/java/com/smartcampus/store/DataStore.java
@@ -10,7 +10,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * In-memory singleton DataStore to act as our "database".
+ * In-memory singleton DataStore to act as the"database".
  * Uses ConcurrentHashMap to ensure thread safety across concurrent JAX-RS requests.
  */
 public class DataStore {
@@ -23,9 +23,9 @@ public class DataStore {
     private final Map<String, List<SensorReading>> sensorReadings = new ConcurrentHashMap<>();
 
     private DataStore() {
-        // Optional: Pre-populate with some initial dummy data for easy testing
+        // Pre-populate with some initial dummy data for easy testing
         Room r1 = new Room("Library Quiet Study", "Library", 3);
-        r1.setId("LIB-301"); // Force ID so sensors dummy data maps correctly
+        r1.setId("LIB-301"); 
         rooms.put(r1.getId(), r1);
         
         Sensor s1 = new Sensor("TEMP-001", "Temperature", "ACTIVE", 21.5, "LIB-301");
